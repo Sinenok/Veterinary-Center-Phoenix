e1e3a30f7a47aa350a42ae584c117a825f2b4e20
Oleg <oleg13fedorov@yandex.ru>
2023-07-10 01:58:01 +0300

    e
    contacts

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./styles/main.css";
 import mainBanner from "./img/main-page/banner-headr.png";
 import Header from "./components/Header";
+import ContactPage from './components/ContactPage'
 
 function App() {
   return (
     <div className="App">
       <Header />
+        <ContactPage/>      
     </div>
   );
 }
