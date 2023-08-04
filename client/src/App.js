900934ce1d4b68e928409998dfe57bcaf65ed7bc
Oleg <oleg13fedorov@yandex.ru>
2023-08-04 03:11:45 +0300

    a
    add

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,13 @@ import "./styles/main.css";
 import mainBanner from "./img/main-page/banner-headr.png";
 import Header from "./components/Header";
 import ContactPage from './components/ContactPage'
+import PriseList from "./components/PriseList";
 
 function App() {
   return (
     <div className="App">
       <Header />
-        <ContactPage/>      
+      <PriseList></PriseList>   
     </div>
   );
 }
---
+++ client/src/components/PriseList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./../styles/oleg.css";
+
+
+function PriseList() {
+    return (
+      <div className="PriseList">
+        <p>начнем</p>
+      </div>
+    );
+  }
+  
+  export default PriseList;
+  
