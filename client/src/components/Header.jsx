926f25737cd03415ed69b8b283b2119d82e88087
Ivan <sinenok.ivan@mail.ru>
2023-08-18 14:44:33 +0300

    full layout of the main page + footer

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,21 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import "./../styles/main.css";
 import mainLogo from "./../img/main-page/logo.svg";
 import mainIcon from "./../img/main-page/Icon.svg";
+import mainIconMedia from "./../img/main-page/Icon-media.svg";
+import hamburgerMenu from "./../img/main-page/hamburger-menu.svg";
+import closeBurger from "./../img/main-page/closeBurger.svg";
+import { Link } from "react-router-dom";
 
-function Header() {
+const Header = ({
+  handleMouseEnter,
+  handleMouseLeave,
+  handleMouseClick,
+  isRotated,
+}) => {
+  const getIconPath = () => {
+    const screenWidth = window.innerWidth;
+    if (screenWidth < 1230) {
+      return mainIconMedia;
+    } else {
+      return mainIcon;
+    }
+  };
+  const screenWidth = window.innerWidth;
+  const toggleBurger = () => {
+    const nav = document.querySelector("#nav");
+    const navBtn = document.querySelector("#navBtn");
+    const navBtnImg = document.querySelector("#navBtnImg");
+    const body = document.querySelector(".body");
+    if (nav.classList.toggle("open")) {
+      body.classList.toggle("open");
+      navBtn.classList.toggle("close");
+      navBtnImg.src = closeBurger;
+    } else {
+      body.classList.toggle("open");
+      navBtn.classList.toggle("close");
+      navBtnImg.src = hamburgerMenu;
+    }
+  };
   return (
     <div className="Header">
-      <nav className="nav">
+      <nav id="nav" className="nav">
         <div className="container">
           <div className="nav__row">
             <div className="nav__logo logo">
-              <img className="logo__img" alt="" src={mainLogo} />
+              <Link to="/">
+                <img className="logo__img" alt="" src={mainLogo} />
+              </Link>
             </div>
             <ul className="nav__list-links list">
-              <li className="list__item">О нас</li>
-              <li className="list__item">Специалисты</li>
               <li className="list__item">
+                <a href="#">О нас</a>
+              </li>
+              <li className="list__item">
+                <a href="#">Специалисты</a>
+              </li>
+              {/* <li className="list__item">
                 <ul className="list-clients">
                   <li className="list-clients__item">Клиентам</li>
                   <li className="list-clients__item">
@@ -26,21 +65,107 @@ function Header() {
                     />
                   </li>
                 </ul>
+              </li> */}
+              <li
+                className="list__item-dropdown list__item"
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+                onClick={handleMouseClick}
+              >
+                <ul className="list-services">
+                  <a
+                    className="link-down"
+                    href={screenWidth > 1230 ? "#" : "#dsad"}
+                  >
+                    <li className="list-services__item">Услуги</li>
+                    <li className="li-icon-nav list-services__item">
+                      <picture>
+                        <source
+                          media="(max-width: 1230px)"
+                          srcSet={mainIconMedia}
+                        />
+                        <img
+                          className={`list-services__item-icon ${
+                            isRotated ? "rotated" : ""
+                          }`}
+                          alt=""
+                          src={mainIcon}
+                        />
+                      </picture>
+                    </li>
+                  </a>
+                </ul>
+                {isRotated && (
+                  <div
+                    className="dropdown-wrapper"
+                    // onMouseEnter={handleMouseEnter}
+                    // onMouseLeave={handleMouseLeave}
+                  >
+                    <ul className="dropdown-list">
+                      <li className="dropdown-list__item">
+                        <Link to="/">Эндоскопия</Link>
+                      </li>
+                      <li className="dropdown-list__item">
+                        <Link to="/a">Репродукция</Link>
+                      </li>
+                      <li className="dropdown-list__item">
+                        <Link to="/">Терапия</Link>
+                      </li>
+                      <li className="dropdown-list__item">
+                        <Link to="/">Неонатология</Link>
+                      </li>
+                      <li className="dropdown-list__item">
+                        <Link to="/">Хирургия</Link>
+                      </li>
+                      <li className="dropdown-list__item">
+                        <Link to="/">Кастрация, стерелизации</Link>
+                      </li>
+                      <li className="dropdown-list__item">
+                        <Link to="/">Кардиология</Link>
+                      </li>
+                      <li className="dropdown-list__item">
+                        <Link to="/">Дерматология</Link>
+                      </li>
+                      <li className="dropdown-list__item">
+                        <Link to="/">Ортопедия и травмотология</Link>
+                      </li>
+                      <li className="dropdown-list__item">
+                        <Link to="/">Пластическая хирургия</Link>
+                      </li>
+                      <li className="dropdown-list__item">
+                        <Link to="/">Операции на голове</Link>
+                      </li>
+                      <li className="dropdown-list__item">
+                        <Link to="/">Стоматология</Link>
+                      </li>
+                      <li className="dropdown-list__item">
+                        <Link to="/">Анастезия и интенсивная терапия</Link>
+                      </li>
+                      <li className="dropdown-list__item">
+                        <Link to="/">Стационар</Link>
+                      </li>
+                      <li className="dropdown-list__item">
+                        <Link to="/">Онкология</Link>
+                      </li>
+                      <li className="dropdown-list__item">
+                        <Link to="/">Вакцинация и чипирование</Link>
+                      </li>
+                      <li className="dropdown-list__item">
+                        <Link to="/">Груминг и стрижка</Link>
+                      </li>
+                      <li className="dropdown-list__item">
+                        <Link to="/">Лабораторная диагностика</Link>
+                      </li>
+                    </ul>
+                  </div>
+                )}
               </li>
               <li className="list__item">
-                <ul className="list-services">
-                  <li className="list-services__item">Услуги</li>
-                  <li className="list-services__item">
-                    <img
-                      className="list-services__item-icon"
-                      alt=""
-                      src={mainIcon}
-                    />
-                  </li>
-                </ul>
+                <a href="#">Цены</a>
               </li>
-              <li className="list__item">Цены</li>
-              <li className="list__item">Контакты</li>
+              <li className="list__item">
+                <a href="#">Контакты</a>
+              </li>
             </ul>
             <ul className="nav__list-info list">
               <li className="list__item-info">
@@ -59,9 +184,27 @@ function Header() {
             </div>
           </div>
         </div>
+        <div className="nav__row-deice">
+          <div className="device-nav__logo logo">
+            <Link to="/">
+              <img className="device-logo__img" alt="" src={mainLogo} />
+            </Link>
+          </div>
+          <ul className="device-list-contacts">
+            <li className="device-list-contacts__number">
+              +7 &#40;812&#41;965-34-50
+            </li>
+            <li className="device-list-contacts__address">
+              пр. Обуховской Обороны, д.195
+            </li>
+          </ul>
+        </div>
+        <button id="navBtn" className="burger-menu-btn" onClick={toggleBurger}>
+          <img id="navBtnImg" src={hamburgerMenu} alt="" />
+        </button>
       </nav>
     </div>
   );
-}
+};
 
 export default Header;
