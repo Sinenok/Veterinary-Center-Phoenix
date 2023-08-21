95f4da0d2f11bf1cf4ecfd71f908dae34cf69f75
Oleg <oleg13fedorov@yandex.ru>
2023-08-21 03:00:49 +0300

    add service

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,16 @@ import Header from "./components/Header";
 import ContactPage from './components/ContactPage'
 import PriseList from "./components/PriseList";
 import PrivacyPolicy from "./components/PrivacyPolicy";
+import ServicePage from "./components/ServicePage";
+
 
 function App() {
   return (
     <div className="App">
+      
       {/* <Header /> */}
-      <PriseList></PriseList>   
+      <ServicePage></ServicePage>
+      {/* <PriseList></PriseList>    */}
       {/* <ContactPage></ContactPage> */}
       {/* <PrivacyPolicy></PrivacyPolicy> */}
     </div>
